e7f9606056d8574d85030d57214066b01b3f6e47
treddeni <vtredd@hotmail.com>
2015-05-13 11:54:14 -0400

    Now using a single database connection

--- db.js
+++ db.js
@@ -1,5 +1,13 @@
+var mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var connection;
+
 var serverName = 'mongodb://localhost';
 var collection = 'tree';
 var url = serverName + '/' + collection;
 
-exports.url = url;
+MongoClient.connect(url, function(err, database)
+{
+  if(err) throw err;
+  exports.connection = database;
+});
