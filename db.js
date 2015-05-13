21668883930bbdc8e9d766da4f774fae1cce9e41
treddeni <vtredd@hotmail.com>
2015-05-13 15:24:23 -0400

    Configured to work with Heroku

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 var mongodb = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
-var connection;
 
 var serverName = 'mongodb://localhost';
 var collection = 'tree';
-var url = serverName + '/' + collection;
-//var url = 'mongodb://hms:hms@ds035162.mongolab.com:35162/hms';
-url = process.env.DB_URL;
+
+var url = process.env.DB_URL;
+
+if(url == undefined)
+{
+  url = serverName + '/' + collection;
+}
 
 MongoClient.connect(url, function(err, database)
 {
