0973ec39e031d19272d7d1bd412ab7fc4215372f
treddeni <vtredd@hotmail.com>
2015-05-13 14:40:46 -0400

    Change db url

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ var connection;
 var serverName = 'mongodb://localhost';
 var collection = 'tree';
 var url = serverName + '/' + collection;
+var url = 'mongodb://hms:hms@ds035162.mongolab.com:35162/hms';
 
 MongoClient.connect(url, function(err, database)
 {
